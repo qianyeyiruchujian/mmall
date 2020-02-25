4ca524379b00cb0e46823fdd36254648a1cb85ee
lygao <1067290341@qq.com>
2020-02-25 20:44:35 +0800

    mmall

---
+++ src/main/java/com/mmall/MmallApplication.java
@@ -0,0 +1,15 @@
+package com.mmall;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.mmall.dao")
+public class MmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmallApplication.class, args);
+    }
+
+}
